9a8ded2b904565c7f647233d7e0ce027d6555e9b
RjH <ryanjayhuffaker@gmail.com>
2022-12-03 10:28:54 -0700

    Improved security

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: "5675b0b21b9f45eda84cc78380127fd1"
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with api'));
+}
+
 const handleImage = (db) => (req, res) => {
 	const { id } = req.body;
 		db('users').where('id', '=', id)
@@ -11,5 +26,6 @@ const handleImage = (db) => (req, res) => {
 }
 
 module.exports = {
+	handleApiCall: handleApiCall,
 	handleImage: handleImage
 };
